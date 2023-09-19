5a23378bb1b07924d85a02b23e45a8c83ff75f15
kornflakes <faglyramschie7@gmail.com>
2023-09-20 01:36:44 +0700

    initial commit

---
+++ src/components/Guene/GnInput/index.ts
@@ -0,0 +1,3 @@
+import GnInput from './GnInput.vue'
+
+export default { GnInput }
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import GnInput from '../components/Guene/GnInput/GnInput.vue';
+</script>
+
+<template>
+  <main class="gn-bg-[#1B2127] gn-min-h-screen gn-p-4">
+    <GnInput></GnInput>
+  </main>
+</template>
